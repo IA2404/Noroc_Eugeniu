95070cefcfae99ada7ba06bf6df95f0e8de67a4d
EugeniuNoroc <eukoron@gmail.com>
2025-03-14 15:15:31 +0200

    Update main.js

--- lab2/main.js
+++ lab2/main.js
@@ -6,21 +6,6 @@
 //merchant_name - название магазина или сервиса.
 //card_type - тип карты (кредитная или дебетовая).
 
-/*const transactions = [
-  {
-    transaction_id: "1",
-    transaction_date: "2019-01-01",
-    transaction_amount: 100.0,
-    transaction_type: "debit",
-    transaction_description: "Payment for groceries",
-    merchant_name: "SuperMart",
-    card_type: "Visa",
-  },
-];
-
-const transactions = [];*/
-
-
 const transactions = [
     [
       {
